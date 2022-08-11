5f0003cc1632bec1d63a3d38a4b943a16582a6f8
Coformatique <coformatique@192.168.1.8>
2022-08-11 23:13:09 +0200

    Adding google auth

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,21 @@ import { UserContextProvider } from './store/user-context'
 import { BrowserRouter } from 'react-router-dom'
 import './i18n'
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
   <React.StrictMode>
-    <BrowserRouter>
-      <UserContextProvider>
-        <App />
-      </UserContextProvider>
-    </BrowserRouter>
+    {/* <GoogleOAuthProvider clientId="87231620963-rd7m2qmjk0nabct0npjeljs7guhvucn0.apps.googleusercontent.com"> */}
+
+
+      <BrowserRouter>
+        <UserContextProvider>
+          <App />
+        </UserContextProvider>
+      </BrowserRouter>
+    {/* </GoogleOAuthProvider>; */}
+
 
   </React.StrictMode>
 )
