4bd6355f4f84fde48bde5895ac2f5dccd7d8db0a
Coformatique <coformatique@192.168.1.8>
2022-09-01 17:48:20 +0200

    Try scanning

--- src/components/ScanQR.tsx
+++ src/components/ScanQR.tsx
@@ -1,34 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 import Modal from './UI/Modal'
 import { useTranslation } from 'react-i18next'
+import { MdQrCodeScanner } from "react-icons/md";
+import { useHistory } from 'react-router-dom'
 
 const ScanQR = (props: any) => {
-    const [data, setData] = useState('No result');
     const [isOpenModal, setIsOpenModal] = useState(false)
     const { t } = useTranslation()
+    const history = useHistory()
 
+
+    useEffect(() => {
+        if (!isOpenModal) {
+            try {
+
+                navigator.getUserMedia({ audio: false, video: true },
+                    function (stream) {
+                        var track = stream.getTracks()[0]; // if only one media track
+                        track.stop();
+                    },
+                    function (error) {
+                        console.log('getUserMedia() error', error);
+                    });
+
+            } catch (e) { }
+        }
+
+    }, [!isOpenModal])
     return (
         <>
             <p className="actions small-btn" onClick={() => setIsOpenModal(true)}>
-                {t('scan')}
+                {t('scan')} <MdQrCodeScanner />
             </p>
 
             <Modal styles={""} open={isOpenModal} close={() => setIsOpenModal(false)}>
 
-                <QrReader
-                    onResult={(result: any, error: any) => {
-                        if (!!result) {
-                            setData(result?.text);
-                        }
+                {isOpenModal &&
+                    <div>
 
-                        if (!!error) {
-                            console.info(error);
-                        }
-                    }}
-                    constraints={{ facingMode: 'environment' }}
-                />
-                <p>{data}</p>
+                        <QrReader
+
+                            onResult={(result: any, error: any) => {
+                                if (!!result) {
+                                    // setData(result?.text);
+                                    return history.replace(result)
+
+                                }
+
+                                if (!!error) {
+                                    console.info(error);
+                                }
+                            }}
+                            constraints={{ facingMode: 'environment' }}
+                        />
+                    </div>
+
+                }
             </Modal>
         </>
     );
